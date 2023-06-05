9b4fbf80cbcaafeaeb26b4f7035ca569e368c56d
Mamuni98 <nabanitasahoo468@gmail.com>
2023-06-05 12:53:00 +0530

    subscribe-and-dispatchActions

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,24 @@
-import classes from './Counter.module.css';
+import { useSelector, useDispatch } from "react-redux";
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const toggleCounterHandler = () => {};
-
+  const counter = useSelector((state) => state.counter);
+  const incrementHandler = () => {
+    dispatch({ type: "IncrementBy5" });
+  };
+  const decrementHandler = () => {
+    dispatch({ type: "DecrementBy5" });
+  };
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment 5</button>
+        <button onClick={decrementHandler}>Decrement 5</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/central-store.js
+++ src/store/central-store.js
@@ -1,11 +1,11 @@
 import { createStore } from "redux";
 
 const dispatchfunction = (state = { counter: 0 }, action) => {
-  if (action.type === "IncrementBy2") {
-    return { counter: state.counter + 2 };
+  if (action.type === "IncrementBy5") {
+    return { counter: state.counter + 5 };
   }
-  if (action.type === "DecrementBy2") {
-    return { counter: state.counter - 2 };
+  if (action.type === "DecrementBy5") {
+    return { counter: state.counter - 5 };
   }
   return state;
 };
